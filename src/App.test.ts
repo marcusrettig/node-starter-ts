a4e494f2ce3020d0f18f6e3a751ee904ccb6ba49
Marcus Rettig <marcus0706@live.se>
2018-05-13 23:52:19 +0200

    Setup jest testing

---
+++ src/App.test.ts
@@ -0,0 +1,13 @@
+import { App } from "./App";
+
+describe("App", () => {
+  let app: App;
+
+  beforeEach(() => {
+    app = new App();
+  });
+
+  test("Message is Hello World!", () => {
+    expect(app.getMessage()).toEqual("Hello World!");
+  });
+});
